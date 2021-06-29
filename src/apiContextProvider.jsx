18b465bb9752d64a92ffeb33d2591da92441ce88
Dylano <dylano.hartman@gmail.com>
2021-06-29 22:23:16 +0200

    cvbcvb

--- src/apiContextProvider.jsx
+++ src/apiContextProvider.jsx
@@ -13,7 +13,7 @@ const ApiContextProvider = (props) => {
 
   useEffect(() => {
     async function loadcopd() {
-      const response = await fetch(siteUrl2, { mode: 'no-cors' })
+      const response = await fetch(siteUrl2)
       if (!response.ok) {
         console.log('error')
         return
